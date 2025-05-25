031aadf3b3c11348084162934259703ea68dc4fb
DevTamakuwala <dev.official2424@gmail.com>
2025-05-25 23:44:36 +0530

    First commit

---
+++ oopDemo.py
@@ -0,0 +1,49 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
+
+def sum1(a = 10, b = 20, *c):
+    res = a + b
+    for i in c:
+        res += i
+    return res
+
+def sum2(a = 10, b = 20, **c):
+    res = a + b
+    for i in c.values():
+        res += i
+    return res
+p1 = Person("John", 36)
+p1.display()
+
+print(sum1(11, 22, 10, 22, 34, 21))
+print(sum2(a=11,b= 22,c= 10,d= 22, e=34, f=21))
+
+
+class Student:
+    def __init__(self):
+        self.name = "Alice"     # public
+        self._age = 20          # protected (convention)
+        self.__marks = 90       # private (name mangled)
+
+    def get_marks(self):
+        return self.__marks
+
+s = Student()
+print(s.name)           # Accessible
+print(s._age)           # Accessible (not recommended)
+# print(s.__marks)      # Error
+print(s.get_marks())    # Correct way to access private data
+
+
+class MyClass:
+    """This is a sample class"""
+    pass
+
+print(MyClass.__doc__)      # "This is a sample class"
+print(MyClass.__name__)     # "MyClass"
+print(MyClass.__module__)   # "__main__"
--- pythonDemo.py
+++ pythonDemo.py
@@ -1,3 +1,11 @@
+"""
+List = [] = mutable
+Tuple = () = immutable
+Dictionary = {"key": value, "key": value} = mutable
+Range = range(10)
+Set = {} = mutable
+String = "" = immutable
+"""
 list1 = [1, 2, 3, 4]
 
 print(list1)
@@ -57,3 +65,23 @@ while i < 10:
     i += 1
     print(i, end=" ")
 
+print()
+fruits = ['apple', 'cherry', 'banana']
+fruits += ['kiwi']
+
+print(fruits)
+print(fruits[1:3])
+fruits.sort()
+print(fruits)
+
+for fruit in fruits:
+    print(fruit)
+
+
+fruits = {"apple", "banana", "cherry", "apple"}
+print(fruits)
+fruits.add("kiwi")
+print(fruits)
+fruits.discard("banana")
+print(fruits)
+print(type(fruits))
